20cd5d91c4470fffc470f81ca167e20e0d3dc8ff
Redha <tataredhaalfath13@gmail.com>
2024-07-24 22:00:37 +0700

    setup dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Building;
+use App\Models\Cctv;
+use App\Models\Floor;
+use App\Models\User;
+use App\Models\UserBuilding;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cookie;
 
@@ -9,8 +14,22 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $user = json_decode(Cookie::get("user"));
-        $title = $user->username;
-        return view("pages.admin.index", compact("title"));
+        $title = "Dashboard Monitoring CCTV";
+        $user = Auth()->user();
+        $buildings = Building::count();
+        $floors = Floor::count();
+        $cctvs = Cctv::count();
+        $users = User::count();
+        $buildingName = "NOT ASSIGN";
+
+        if ($user->role == "operator_gedung") {
+            $userBuilding = UserBuilding::with("building")->where("user_id", $user->id)->first();
+            if ($userBuilding) {
+                $buildingName = $userBuilding->building->name;
+                $floor = Floor::where("building_id", $userBuilding->building->id)->count();
+            }
+        }
+
+        return view("pages.admin.index", compact("title", "buildings", "floors", "cctvs", "users", "buildingName", "user"));
     }
 }
--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -2,20 +2,65 @@
 @section('title', $title)
 @section('content')
     <div class="row">
-        {{-- <div class="col-6 col-sm-6 col-md-3">
+        @if ($user->role != 'operator_gedung')
+            <div class="col-6 col-sm-6 col-md-3">
+                <div class="card card-stats card-round">
+                    <div class="card-body">
+                        <a href="{{ route('building') }}">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="flaticon-store text-info"></i>
+                                    </div>
+                                </div>
+                                <div class="col-7 col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Gedung</p>
+                                        <h4 class="card-title">{{ $buildings }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        @else
+            <div class="col-6 col-sm-6 col-md-3">
+                <div class="card card-stats card-round">
+                    <div class="card-body">
+                        <a href="{{ route('building') }}">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="flaticon-store text-info"></i>
+                                    </div>
+                                </div>
+                                <div class="col-7 col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Gedung</p>
+                                        <h4 class="card-title">{{ $buildingName }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        @endif
+        <div class="col-6 col-sm-6 col-md-3">
             <div class="card card-stats card-round">
-                <div class="card-body">
-                    <a href="{{ route('article') }}">
+                <div class="card-body ">
+                    <a href="{{ route('floor') }}">
                         <div class="row">
                             <div class="col-5">
                                 <div class="icon-big text-center">
-                                    <i class="flaticon-agenda text-info"></i>
+                                    <i class="flaticon-layers text-success"></i>
                                 </div>
                             </div>
                             <div class="col-7 col-stats">
                                 <div class="numbers">
-                                    <p class="card-category">Artikel</p>
-                                    <h4 class="card-title">{{ $articles }}</h4>
+                                    <p class="card-category">Lantai</p>
+                                    <h4 class="card-title">{{ $floors }}</h4>
                                 </div>
                             </div>
                         </div>
@@ -26,17 +71,17 @@
         <div class="col-6 col-sm-6 col-md-3">
             <div class="card card-stats card-round">
                 <div class="card-body ">
-                    <a href="{{ route('product') }}">
+                    <a href="{{ route('cctv') }}">
                         <div class="row">
                             <div class="col-5">
                                 <div class="icon-big text-center">
-                                    <i class="flaticon-box-1 text-success"></i>
+                                    <i class="flaticon-photo-camera text-primary"></i>
                                 </div>
                             </div>
                             <div class="col-7 col-stats">
                                 <div class="numbers">
-                                    <p class="card-category">Produk</p>
-                                    <h4 class="card-title">{{ $products }}</h4>
+                                    <p class="card-category">CCTV</p>
+                                    <h4 class="card-title">{{ $cctvs }}</h4>
                                 </div>
                             </div>
                         </div>
@@ -44,132 +89,29 @@
                 </div>
             </div>
         </div>
-        <div class="col-6 col-sm-6 col-md-3">
-            <div class="card card-stats card-round">
-                <div class="card-body ">
-                    <a href="{{ route('slide') }}">
-                        <div class="row">
-                            <div class="col-5">
-                                <div class="icon-big text-center">
-                                    <i class="flaticon-web text-primary"></i>
+        @if ($user->role != 'operator_gedung')
+            <div class="col-6 col-sm-6 col-md-3">
+                <div class="card card-stats card-round">
+                    <div class="card-body ">
+                        <a href="{{ route('user') }}">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="flaticon-users text-info"></i>
+                                    </div>
+                                </div>
+                                <div class="col-7 col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Pengguna</p>
+                                        <h4 class="card-title">{{ $users }}</h4>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-7 col-stats">
-                                <div class="numbers">
-                                    <p class="card-category">Slide</p>
-                                    <h4 class="card-title">{{ $slides }}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-6 col-sm-6 col-md-3">
-            <div class="card card-stats card-round">
-                <div class="card-body ">
-                    <a href="{{ route('gallery') }}">
-                        <div class="row">
-                            <div class="col-5">
-                                <div class="icon-big text-center">
-                                    <i class="flaticon-picture text-info"></i>
-                                </div>
-                            </div>
-                            <div class="col-7 col-stats">
-                                <div class="numbers">
-                                    <p class="card-category">Galeri</p>
-                                    <h4 class="card-title">{{ $galleries }}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-sm-6 col-md-3">
-            <div class="card card-stats card-round">
-                <div class="card-body ">
-                    <a href="{{ route('team') }}">
-                        <div class="row">
-                            <div class="col-5">
-                                <div class="icon-big text-center">
-                                    <i class="flaticon-user-5 text-primary"></i>
-                                </div>
-                            </div>
-                            <div class="col-7 col-stats">
-                                <div class="numbers">
-                                    <p class="card-category">Team</p>
-                                    <h4 class="card-title">{{ $teams }}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-sm-6 col-md-3">
-            <div class="card card-stats card-round">
-                <div class="card-body ">
-                    <a href="{{ route('review') }}">
-                        <div class="row">
-                            <div class="col-5">
-                                <div class="icon-big text-center">
-                                    <i class="flaticon-chat-5 text-success"></i>
-                                </div>
-                            </div>
-                            <div class="col-7 col-stats">
-                                <div class="numbers">
-                                    <p class="card-category">Review</p>
-                                    <h4 class="card-title">{{ $reviews }}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-sm-6 col-md-3">
-            <div class="card card-stats card-round">
-                <div class="card-body ">
-                    <a href="{{ route('contact') }}">
-                        <div class="row">
-                            <div class="col-5">
-                                <div class="icon-big text-center">
-                                    <i class="flaticon-chat-1 text-info"></i>
-                                </div>
-                            </div>
-                            <div class="col-7 col-stats">
-                                <div class="numbers">
-                                    <p class="card-category">Pesan Masuk</p>
-                                    <h4 class="card-title">{{ $messages }}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-sm-6 col-md-3">
-            <div class="card card-stats card-round">
-                <div class="card-body ">
-                    <a href="{{ route('user') }}">
-                        <div class="row">
-                            <div class="col-5">
-                                <div class="icon-big text-center">
-                                    <i class="flaticon-users text-primary"></i>
-                                </div>
-                            </div>
-                            <div class="col-7 col-stats">
-                                <div class="numbers">
-                                    <p class="card-category">Pengguna</p>
-                                    <h4 class="card-title">{{ $users }}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div> --}}
+        @endif
     </div>
 
 @endsection

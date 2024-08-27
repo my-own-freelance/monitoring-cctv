eb42be0484693983eadb1406c15d12c2a3f00153
Redha <tataredhaalfath13@gmail.com>
2024-08-27 17:09:15 +0700

    add recaptcha

--- app/Http/Controllers/WebAuthController.php
+++ app/Http/Controllers/WebAuthController.php
@@ -43,9 +43,28 @@ class WebAuthController extends Controller
         return $this->authService->register($request);
     }
 
+    function validateCaptcha($captchaResponse)
+    {
+        $secretKey = env('recaptcha2.secret');
+        $verifyUrl = 'https://www.google.com/recaptcha/api/siteverify';
+
+        $response = file_get_contents($verifyUrl . '?secret=' . $secretKey . '&response=' . $captchaResponse);
+        $responseKeys = json_decode($response, true);
+        return $responseKeys;
+        return isset($responseKeys["success"]) && $responseKeys["success"] === true;
+    }
+
+
     public function validateLogin(Request $request)
     {
         try {
+
+            $captchaResponse = $request->input('g-recaptcha-response');
+
+            if (!$this->validateCaptcha($captchaResponse)) {
+                return response()->json(['message' => 'Captcha tidak valid.'], 422);
+            }
+
             $rules = [
                 "username" => "required|string",
                 "password" => "required|string",
